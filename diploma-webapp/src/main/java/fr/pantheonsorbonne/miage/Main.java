a7b318db5a6a8d5d398d6254570dbd5bab3eae2b
linaamayro <linaamayri@gmail.com>
2018-12-03 12:42:04 +0100

    Bug 1 coorection

--- diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
+++ diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -66,7 +66,7 @@ public class Main {
 		Iterables.addAll(students, repo);
 
 		for (int i = 0; i < students.size(); i++) {
-			if (i == studentId) {
+			if (students.get(i).getId() == studentId) {
 				return students.get(i);
 			}
 		}
