d40b172962aed7e3a3c3c262c548e4c17c423283
linaamayro <linaamayri@gmail.com>
2019-01-24 12:40:03 +0100

    Correction with Sonarlint & Error

--- diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
+++ diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -2,34 +2,17 @@ package fr.pantheonsorbonne.miage;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URI;
-import java.net.URISyntaxException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Date;
-import java.util.Locale;
 import java.util.NoSuchElementException;
-import java.util.logging.Logger;
-
-import javax.servlet.Servlet;
-import javax.servlet.ServletRegistration;
 
 import org.glassfish.grizzly.http.io.NIOOutputStream;
 import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.Request;
 import org.glassfish.grizzly.http.server.Response;
-import org.glassfish.grizzly.http.util.ContentType;
-import org.glassfish.grizzly.servlet.WebappContext;
-import org.glassfish.jersey.grizzly2.servlet.GrizzlyWebContainerFactory;
 
-import com.google.common.collect.Collections2;
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Streams;
 import com.google.common.io.ByteStreams;
 
 /**
@@ -37,12 +20,9 @@ import com.google.common.io.ByteStreams;
  *
  */
 public class Main {
-	public static final String HOST = "localhost";
-	public static final int PORT = 7000;
-	private static final Logger logger = Logger.getLogger(Main.class.getName());
 	private static StudentRepository studentRepo = StudentRepository.withDB("src/main/resources/students.db");
 
-	public static void main(String[] args) throws IOException, URISyntaxException {
+	public static void main(String[] args) {
 
 		HttpServer server = HttpServer.createSimpleServer();
 		addRootPath(server, "/home");
@@ -60,14 +40,14 @@ public class Main {
 		}
 	}
 
-	protected static Student getStudentData(int studentId, StudentRepository repo) {
+	private static Student getStudentData(int studentId, StudentRepository repo) {
 		// create an arrayList of the students, because iterables are too hard
 		ArrayList<Student> students = new ArrayList<>();
 		Iterables.addAll(students, repo);
 
-		for (int i = 0; i < students.size(); i++) {
+		for (int i = 1 i <= students.size(); i++) {
 			if (students.get(i).getId() == studentId) {
-				return students.get(i);
+				return students.get(i-1);
 			}
 		}
 
@@ -75,7 +55,7 @@ public class Main {
 
 	}
 
-	protected static void handleResponse(Response response, int studentId) throws IOException {
+	private static void handleResponse(Response response, int studentId) throws IOException {
 
 		response.setContentType("application/pdf");
 
@@ -91,7 +71,7 @@ public class Main {
 		return;
 	}
 
-	protected static void addDiplomaPath(HttpServer server, String path) {
+	private static void addDiplomaPath(HttpServer server, String path) {
 		server.getServerConfiguration().addHttpHandler(new HttpHandler() {
 			public void service(Request request, Response response) throws Exception {
 				// get the id of the student
@@ -106,7 +86,7 @@ public class Main {
 		}, path);
 	}
 
-	protected static void addRootPath(HttpServer server, String path) {
+	private static void addRootPath(HttpServer server, String path) {
 		server.getServerConfiguration().addHttpHandler(new HttpHandler() {
 			public void service(Request request, Response response) throws Exception {
 
@@ -119,7 +99,6 @@ public class Main {
 					sb.append("</li>");
 				}
 
-				sb.append("</ul></body></html>");
 				response.setContentType("text/html; charset=utf-8");
 				response.setContentLength(sb.length());
 				response.getWriter().write(sb.toString());
